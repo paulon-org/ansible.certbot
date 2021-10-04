16601a61042eb9ce3310b3fc8d6b4ba0c51fe6e1
Michaël Paulon <michael@paulon.org>
2021-10-04 21:41:30 +0200

    patch auth.sh

--- templates/etc/letsencrypt/scripts/auth.sh
+++ templates/etc/letsencrypt/scripts/auth.sh
@@ -4,9 +4,9 @@ set -e
 set -u
 set -o pipefail
 
-NSUPDATE="nsupdate -k /etc/letsencrypt/scripts/{{ item }}-key.conf"
-DNSSERVER="{{ global_certbot.certs[item].server }}"
-ZONE="{{ global_certbot.certs[item].zone }}"
+NSUPDATE="nsupdate -k /etc/letsencrypt/scripts/{{ certbot.certname }}-key.conf"
+DNSSERVER="{{ certbot.dns.server }}"
+ZONE="{{ certbot.dns.zone }}"
 TTL=300
 
-printf "server %s\nzone %s.\nupdate add {{ global_certbot.certs[item].record }} %d in TXT \"%s\"\nsend\n" "${DNSSERVER}" "${ZONE}" "${TTL}" "${CERTBOT_VALIDATION}" | $NSUPDATE
+printf "server %s\nzone %s.\nupdate add {{ certbot.dns.record }} %d in TXT \"%s\"\nsend\n" "${DNSSERVER}" "${ZONE}" "${TTL}" "${CERTBOT_VALIDATION}" | $NSUPDATE
