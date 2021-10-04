128692bef67fd3f30d4c0905e8bbee064d3f4d59
Michaël Paulon <michael@paulon.org>
2021-10-05 00:51:44 +0200

    update ttl in clean.sh

--- templates/etc/letsencrypt/scripts/clean.sh
+++ templates/etc/letsencrypt/scripts/clean.sh
@@ -7,6 +7,6 @@ set -o pipefail
 NSUPDATE="nsupdate -k /etc/letsencrypt/scripts/{{ certbot.certname }}-key.conf"
 DNSSERVER="{{ certbot.dns.server }}"
 ZONE="{{ certbot.dns.zone }}"
-TTL=300
+TTL=1
 
 printf "server %s\nzone %s.\nupdate delete {{ certbot.dns.record }} %d in TXT \"%s\"\nsend\n" "${DNSSERVER}" "${ZONE}" "${TTL}" "${CERTBOT_VALIDATION}" | $NSUPDATE
